7868287a31be970173693ca71d42d72de6a3bdfc
brendan brown <brendanbrown27@gmail.com>
2017-04-06 09:10:38 +0200

    routing error agan

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', ['ngRoute']);
 app.config(function ($routeProvider, $locationProvider) {
 	$routeProvider
 		.when("/", {
-			templateUrl: "/home.html"
+			templateUrl: "/highlights.html"
 		})
 		.when("/about", {
 			templateUrl: "about.html"
